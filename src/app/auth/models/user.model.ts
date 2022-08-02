16390e1cff589f6d914bbad78e22c5449d898d3d
Maurice Oluoch <maurice.oluoch@8teq.co.ke>
2022-08-02 11:23:53 +0300

    latest changes on notifications

--- src/app/auth/models/user.model.ts
+++ src/app/auth/models/user.model.ts
@@ -1,3 +1,4 @@
+import {strict} from 'assert';
 
 export interface UserModel {
   user: {
@@ -13,4 +14,59 @@ export interface UserModel {
     access: string,
     refresh: string
   };
+}
+
+export interface ZulipUsersResponse {
+  result: string;
+  msg: string;
+  members: ZulipSingleUser[];
+}
+
+export interface ZulipSingleUser {
+  email: string;
+  user_id: number;
+  avatar_version: number;
+  is_admin: boolean;
+  is_owner: boolean;
+  is_guest: boolean;
+  is_billing_admin: boolean;
+  role: number;
+  is_bot: number;
+  full_name: string;
+  timezone: string;
+  is_active: boolean;
+  date_joined: string;
+  avatar_url: string | null;
+}
+
+export interface PresentUsersResponse {
+  msg: string;
+  result: string;
+  members: SinglePresentUser[];
+}
+
+export interface SinglePresentUser {
+  role: number;
+  email: string;
+  is_bot: boolean;
+  user_id: number;
+  is_admin: boolean;
+  is_owner: boolean;
+  presence?: {
+    website: {
+      status: string;
+      timestamp: number;
+    },
+    aggregated: {
+      status: string;
+      timestamp: number;
+    }
+  };
+  timezone: string;
+  full_name: string;
+  is_active: boolean;
+  avatar_url: string | null;
+  date_joined: string;
+  avatar_version: number;
+  is_billing_admin: boolean;
 }
