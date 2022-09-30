7b9d3521b7ccdff4756dda175458914ba4515958
maurice_oluoch <oluochstudy@gmail.com>
2022-09-30 11:00:30 +0300

    latest changes

--- src/app/dashboard/messaging/right-panel/right-panel.component.ts
+++ src/app/dashboard/messaging/right-panel/right-panel.component.ts
@@ -16,10 +16,6 @@ export class RightPanelComponent implements OnInit {
   @Input() userContent!: string;
   @Output() rightPanelEvent = new EventEmitter<string>();
 
-  zulipUsers: ZulipSingleUser[] = [];
-  zulipUsersSubject$ = new BehaviorSubject(this.zulipUsers);
-  zulipUsersObservable = this.zulipUsersSubject$.asObservable();
-
   constructor(
     private store: Store<AppState>
   ) { }
@@ -29,9 +25,6 @@ export class RightPanelComponent implements OnInit {
   }
 
   initPage(): void {
-    setTimeout(() => {
-      this.privateUnreadMessage();
-    }, 1000);
     this.defaultUser();
   }
 
@@ -39,35 +32,6 @@ export class RightPanelComponent implements OnInit {
     if (!this.userContent) {
       this.userContent = 'all_users';
     }
-  }
-
-  privateUnreadMessage(): void {
-    const zulipUsers: ZulipSingleUser[] = [];
-    const unique: number[] = [];
-
-    this.allUsers$.subscribe(
-      (users: ZulipSingleUser[]) => {
-        users.map((user: ZulipSingleUser) => {
-
-          this.store.select(getPrivateUnread).subscribe(
-            (messages: SingleMessageModel[]) => {
-              messages.map((message: SingleMessageModel) => {
-                if (unique.includes(user.user_id)) { return; }
-                if (+message.sender_id === +user.user_id) {
-                  user.unread = user.unread ? user.unread += 1 : user.unread = 0;
-                  console.log('Sender ==>>', user);
-                  console.log('Message ===>>', message);
-                }
-                zulipUsers.push(user);
-                this.zulipUsersSubject$.next(zulipUsers);
-                unique.push(user.user_id);
-              });
-            }
-          );
-
-        });
-      }
-    );
   }
 
   rightPanelTypeListener($event: any): void {
